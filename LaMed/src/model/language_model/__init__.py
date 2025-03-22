06d592135e091e8f243bbe76379032da63070363
Snivellus Snape <21307140051@m.fudan.edu.cn>
2025-03-22 21:38:20 +0800

    Update __init__.py

--- LaMed/src/model/language_model/__init__.py
+++ LaMed/src/model/language_model/__init__.py
@@ -1,7 +1,2 @@
 from .lamed_llama import LamedLlamaForCausalLM
-from .lamed_phi3 import LamedPhi3ForCausalLM
-
-__all__ = [
-    "LamedLlamaForCausalLM",
-    "LamedPhi3ForCausalLM"
-]
+# from .lamed_phi3 import LamedPhi3ForCausalLM
