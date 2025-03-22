07df34e24fde57d929bd2ec985f6a91220cfa1d6
Snivellus Snape <21307140051@m.fudan.edu.cn>
2025-03-22 20:45:35 +0800

    debug LaMed Not Found

--- LaMed/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import dataset
-from . import demo
-from . import model
-from . import train
-from . import utils
---
+++ LaMed/src/__init__.py
@@ -0,0 +1,5 @@
+from . import dataset
+from . import demo
+from . import model
+from . import train
+from . import utils
